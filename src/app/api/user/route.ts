db48053d30c838419f8f2fb4ab6672d7734dba1f
Bourgie69 <batzorig.chinbat19@gmail.com>
2025-12-24 22:26:56 +0800

    POSTs User

--- src/app/api/user/route.ts
+++ src/app/api/user/route.ts
@@ -1,24 +1,28 @@
-
-import prisma from "@/lib/prisma";
 import { auth, currentUser } from "@clerk/nextjs/server";
-import { NextRequest, NextResponse } from "next/server";
+import prisma from "@/lib/prisma";
+import { NextResponse } from "next/server";
 
-export const POST = async (req: NextRequest) => {
-    const { userId } = await auth()
-    if (!userId) return new Response("Unauthorized", { status: 401 })
+export async function POST() {
+  const { userId } = await auth();
+  if (!userId) {
+    return new NextResponse("Not signed in", { status: 401 });
+  }
 
-    const user = await currentUser()
-    if (!user) return new Response("User not found", { status: 401 })
+  const user = await currentUser();
+  if (!user) {
+    return new NextResponse("User not found", { status: 404 });
+  }
 
-    await prisma.user.upsert({
-        where: { id: userId },
-        update: {},
-        create: {
-            id: userId,
-            email: user.emailAddresses[0].emailAddress,
-            name: user.fullName
-        }
-    });
+  await prisma.user.upsert({
+    where: { id: userId },
+    update: {},
+    create: {
+      id: userId,
+      email: user.emailAddresses[0].emailAddress,
+      name: user.firstName,
+      clerkId: user.id
+    },
+  });
 
-    return NextResponse.json({ ok: true })
+  return NextResponse.json({ ok: true });
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,25 @@
+"use client"
+
+import { useEffect } from "react";
 import Nav from "./_components/Nav/Nav";
 import QuizGen from "./_components/QuizGen/QuizGen";
 import SideBar from "./_components/Sidebar/SideBar";
 
 export default function Home() {
+
+  useEffect(() => {
+    const createUser = async () => {
+      await fetch("/api/user", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json"
+        },
+      })
+    }
+    
+    createUser()
+  }, [])
+
   return (
     <div>
       {/* <Nav/> */}
